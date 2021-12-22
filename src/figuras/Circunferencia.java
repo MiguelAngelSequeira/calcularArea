be29b81dd03aa37b932ebd02e50dde8a4bb2ac61
Miguel Sequeira <miguelangelsequeirasarmiento@alumno.ieselrincon.es>
2021-12-22 19:50:27 +0000

    Commit después de comentar el proyecto

--- src/figuras/Circunferencia.java
+++ src/figuras/Circunferencia.java
@@ -1,24 +1,52 @@
 package figuras;
 
+/**
+ * Dado un radio, podemos comparar el radio de la circunferencia con otra,
+ * mostrar por pantalla el diámetro, el color y el área de dicha circunferencia
+ * @author Miguel Ángel Sequeira Sarmiento
+ * @version 1.0
+ */
 public class Circunferencia {
+    /**
+     * Radio de la circunferencia
+     */
     private double radio;
+    /**
+     * Color de la circunferencia
+     */
     private String color;
 
+    /**
+     * Inicializa y asigna el radio a la circunferencia
+     * @param radio: Radio de la circunferencia
+     */
     public Circunferencia(double radio) {
 
         this.radio = radio;
     }
 
+    /**
+     * Getter, obtiene el radio de la circunferencia
+     * @return radio: devuelve el radio de la circunferencia
+     */
     public double getRadio() {
 
         return radio;
     }
 
+    /**
+     * Setter, asigna el radio a la circunferencia
+     * @param radio: Asigna el radio a la circunferencia
+     */
     public void setRadio(double radio) {
 
         this.radio = radio;
     }
 
+    /**
+     * Inicializa color a rojo y presenta por pantalla el diámetro, el color y
+     * el área de la circunferencia
+     */
     public void imprimir() {
         color = "rojo";
         System.out.println("Diámetro: " + 2 * radio);
@@ -27,6 +55,13 @@ public class Circunferencia {
         System.out.println(area);
     }
 
+    /**
+     * Compara el radio con el de otra circunferencia
+     * @param considerarDecimales: True si de deben tener en cuenta los decimales.
+     *                           False para tener en cuenta sólo valores absolutos
+     * @param otro: Circunferencia con la que queremos comprar
+     * @return True si sin iguales o false si son distintas
+     */
     public boolean esIgual(boolean considerarDecimales, Circunferencia otro) {
         double radio1 = this.radio;
         double radio2 = otro.getRadio();
--- src/figuras/Test.java
+++ src/figuras/Test.java
@@ -1,14 +1,29 @@
 package figuras;
 
+/**
+ * Clase principal del proyecto
+ * @author Miguel Ángel Sequeira Sarmiento
+ * @version 1.0
+ */
 public class Test {
+
     public static void main(String[] args) {
+        //Creamos tres circunferencias
         Circunferencia c1 = new Circunferencia(5.5);
         Circunferencia c2 = new Circunferencia(10.1);
         Circunferencia c3 = new Circunferencia(10.9);
 
+        /**
+         * Comparamos la circunferencia c2 con la c3 sin tener encuenta
+         * los decimales
+         */
         if (c2.esIgual(false, c3))
             System.out.println("c2 y c3: iguales sin considerar decimales");
 
+        /**
+         * comparamos la circunferencia c2 con la c3 teniendo en cuenta
+         * los decimales
+         */
         if (c2.esIgual(true, c3))
             System.out.println("c2 y c3: iguales considerando decimales");
     }
